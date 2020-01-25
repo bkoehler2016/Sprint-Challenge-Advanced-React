656ab24afb45070a1c447b39ab719836de934486
Ben Koehler <bkoehler85282@gmail.com>
2020-01-24 19:37:47 -0700

    received data back and got test to run

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,33 @@
 import React from "react";
-
 import "./App.css";
+import PlayerList from "./Components/PlayerList";
+import NavBar from "./Components/NavBar";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Welcome To </h1>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      player: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:5000/api/players")
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ player: data });
+      })
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <NavBar />
+        <PlayerList players={this.state.player} />
+      </div>
+    );
+  }
 }
 
 export default App;
